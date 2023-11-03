f82215ff43ea42ace63aa095a20c17bab2dea466
RasTado <104467363+RasTado@users.noreply.github.com>
2023-11-03 16:11:44 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"/HW_react_forms/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
